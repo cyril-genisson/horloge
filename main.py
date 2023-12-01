2d7f5d3ee3e18da554d7ca1e2a7042ac0772c834
Cyril GENISSON <144389820+cyril-genisson@users.noreply.github.com>
2023-12-01 20:10:44 +0100

    Version complète

--- main.py
+++ main.py
@@ -23,54 +23,73 @@
 # m : passer du mode 24h au mode 12h (AM/PM) 
 #
 # je n'ai pas eu le temps de concevoir l'alarme avec l'affichage
-# du pop-up. En effet je viens de me rendre compte que j'avais mal
+# d'un popup. En effet je viens de me rendre compte que j'avais mal
 # conçu le code. La classe Clock gère l'intégralité de l'affichage
 # alors que j'aurais dû la créer comme un widget urwid pour pouvoir ajouter
 # de nouveaux composants comme un menu. Je reprendrais ce travail pour en avoir
-# une version beaucoup convenable.
+# une version plus convenable. Donc à défaut, je configure une alarme dès le
+# lancement du programme. Bon, c'est pas beau mais ça marche...
 
 import time
 import urwid
 
 
 class Clock:
-    def __init__(self):
+    def __init__(self, alarm=None):
         self.format = "%H:%M:%S"
-        self.configure_clock()
+        self.alarm = alarm
         self.state = {"MODE": False, "ALARM": False, "BREAK": False}
+        if self.alarm:
+            self.text_alarm = self.alarm[1]
+            self.hour_alarm24 = f"{int(self.alarm[0][0]):02d}:{int(self.alarm[0][1]):02d}:{int(self.alarm[0][2]):02d}"
+            if int(self.alarm[0][0]) < 12:
+                self.hour_alarm12 = f"{int(self.alarm[0][0]):02d}:{int(self.alarm[0][1]):02d}:{int(self.alarm[0][2]):02d} AM"
+            else:
+                self.hour_alarm12 = f"{int(self.alarm[0][0]) % 12:02d}:{int(self.alarm[0][1]):02d}:{int(self.alarm[0][2]):02d} PM"
+            self.hour_alarm_str = self.hour_alarm24
+        else:
+            self.hour_alarm_str = None
+        self.configure_clock()
         self.palette = [("clock", "dark blue", "")]
         self.loop = urwid.MainLoop(
                 self.clock,
                 self.palette,
                 unhandled_input=self.unhandled_input)
         self.loop.set_alarm_in(0.1, self.update)
-        self.hours = ('00', '01', '02', '03', '04', '05',
-                      '06', '07', '08', '09', '10', '11',
-                      '12', '13', '14', '15', '16', '17',
-                      '18', '19', '20', '21', '22', '23')
-        self.minutes = ('00', '01', '02', '03', '04', '05', '06', '07', '08', '09'
-                        '10', '11', '12', '13', '14', '15', '16', '17', '18', '19',
-                        '20', '21', '22', '23', '24', '25', '26', '27', '28', '29',
-                        '30', '31', '32', '33', '34', '35', '36', '37', '38', '39',
-                        '40', '41', '42', '43', '44', '45', '46', '47', '48', '49',
-                        '50', '51', '52', '53', '54', '55', '56', '57', '58', '59')
-        self.seconds = ('00', '01', '02', '03', '04', '05', '06', '07', '08', '09'
-                        '10', '11', '12', '13', '14', '15', '16', '17', '18', '19',
-                        '20', '21', '22', '23', '24', '25', '26', '27', '28', '29',
-                        '30', '31', '32', '33', '34', '35', '36', '37', '38', '39',
-                        '40', '41', '42', '43', '44', '45', '46', '47', '48', '49',
-                        '50', '51', '52', '53', '54', '55', '56', '57', '58', '59')
+
 
     def update(self, loop=None, user_data=None):
         if not self.state["BREAK"]:
             self.configure_clock()
-            loop.widget = self.clock
-            loop.set_alarm_in(0.1, self.update)
+            if self.state["ALARM"]:
+                loop.widget = self.clock
+                loop.set_alarm_in(5, self.update)
+            else:
+                loop.widget = self.clock
+                loop.set_alarm_in(0.1, self.update)
         else:
             loop.set_alarm_in(0.1, self.update)
 
+
     def configure_clock(self):
         text = time.strftime(self.format)
+        if text == self.hour_alarm_str:
+            self.state["ALARM"] = True
+            text = self.text_alarm
+            font = urwid.font.HalfBlock7x7Font()
+            self.text_clock = urwid.BigText(text, font)
+            self.clock = urwid.Filler(
+                urwid.AttrMap(
+                    urwid.Padding(
+                        self.text_clock,
+                        "center",
+                        width="clip"
+                    ), "clock"
+                ), "middle"
+            )
+        else:
+            self.state["ALARM"] = False
+
         font = urwid.font.HalfBlock7x7Font()
         self.text_clock = urwid.BigText(text, font)
         self.clock = urwid.Filler(
@@ -83,8 +102,10 @@ class Clock:
                     ), "middle"
                 )
 
+
     def start(self):
         self.loop.run()
+
 
     def unhandled_input(self, key):
         if key in ("q", "Q"):
@@ -93,9 +114,14 @@ class Clock:
             if not self.state["MODE"]:
                 self.state["MODE"] = True
                 self.format = "%I:%M:%S %p"
+                if self.alarm:
+                    self.hour_alarm_str = self.hour_alarm12
             else:
                 self.state["MODE"] = False
                 self.format = "%H:%M:%S"
+                if self.alarm:
+                    self.hour_alarm_str = self.hour_alarm24
+
         if key in ("p", "P"):
             if not self.state["BREAK"]:
                 self.state["BREAK"] = True
@@ -103,8 +129,21 @@ class Clock:
                 self.state["BREAK"] = False
 
 
+def alarm():
+    if input("Do you want a alarm [y/n]: ") == 'y':
+        alarm_time = input("Input time format 24h hh:mm:ss): ")
+        alarm_title = input("Title [ALARM]: ")
+        if not len(alarm_title):
+            alarm_title = "ALARM"
+        return (alarm_time.split(":"), alarm_title)
+    else:
+        return None
+
+
 def main():
-    clock = Clock()
+    timer = alarm()
+    print(timer)
+    clock = Clock(alarm=timer)
     clock.start()
 
 
